7ce826af641e1ef42a28c7b13800b0b40b8e0b6d
Moin Uddin Maruf <69745321+MarufHimalay@users.noreply.github.com>
2021-10-03 23:24:39 +0600

    Update Home.js

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -5,7 +5,7 @@ const Home = () => {
     const [countries, setCountries] = useState([]);
 
   useEffect(() => {
-    fetch('https://restcountries.eu/rest/v2/all')
+    fetch('https://restcountries.com/v2/all')
       .then(res => res.json())
       .then(data => setCountries(data))
   }, [])
